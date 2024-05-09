73ef0217a7e5742da969cac667e14298132fe73f
Roland <regacho800@gmail.com>
2024-05-09 17:01:52 +0800

    working websocket server

--- index.js
+++ index.js
@@ -6,27 +6,43 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const { v4: uuidv4 } = require('uuid');
+let webSockets = {}
 
-wss.on('connection', (ws) => {
-    let id = uuidv4();
-    console.log(`Client ${id} connected`);
-
-    ws.on('connect', (id) => {
-        ws.send(id)
+wss.on('connection', function (webSocket, req) {
+    var userID = req.url.substring(1)
+    webSockets[userID] = webSocket
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
+  
+    // Forward Message
+    //
+    // Receive               Example
+    // [toUserID, text]      [2, "Hello, World!"]
+    //
+    // Send                  Example
+    // [fromUserID, text]    [1, "Hello, World!"]
+    webSocket.on('message', function(message) {
+      console.log('received from ' + userID + ': ' + message)
+      var messageArray = JSON.parse(message)
+      let splitted_id = messageArray[0]
+      let index = ""
+      for(let items in webSockets) {
+        if(items.split("-")[5] == splitted_id) {
+            index = items
+        }
+      }
+      var toUserWebSocket = webSockets[index]
+      if (toUserWebSocket != "") {
+        console.log('sent to ' + index + ': ' + JSON.stringify(messageArray))
+        messageArray[0] = index
+        toUserWebSocket.send(JSON.stringify(messageArray))
+      }
     })
 
-    ws.emit('connect', id)
-
-    ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        ws.send(`You sent: ${message}`);
+    webSocket.on('close', () => {
+        delete webSockets[userID]
+        console.log('deleted: ' + userID)
     });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
+})
 
 server.listen(3000, () => {
     console.log('Server listening on port 3000');
